3beea8a10f22e3ce4c120b71cbf89fc52be47300
Flo Slv <florent3969@hotmail.fr>
2023-02-23 15:08:49 +0100

    🟦 feat: WIP - add lateral Cart

---
+++ frontend/src/components/Cart.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+
+const Cart = ({ openCart, closeCart }) => {
+  const [isOpen, setIsOpen] = useState(openCart);
+
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setIsOpen(open);
+    closeCart();
+  };
+
+  return (
+    <div>
+      <Drawer
+        anchor={"right"}
+        open={isOpen}
+        onClose={toggleDrawer(Boolean(false))}
+      >
+        <Box
+          role="presentation"
+          onClick={toggleDrawer(Boolean(false))}
+          onKeyDown={toggleDrawer(Boolean(false))}
+        >
+          <List>
+            {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+
+          <Divider />
+
+          <List>
+            {["All mail", "Trash", "Spam"].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Drawer>
+    </div>
+  );
+};
+
+export default Cart;
