55a02c7e446dab2ca17003d120a2af59b0bc9a24
Flo Slv <florent3969@hotmail.fr>
2023-02-24 16:19:56 +0100

    🟦 feat: frontend - card per product

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@apollo/client";
 import clsx from "clsx";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 import GET_PRODUCTS_BY_CATEGORY from "../graphql/queries/getProductsByCategory.js";
 
@@ -24,14 +25,42 @@ const Products = ({ cat }) => {
     >
       {data &&
         data?.getProductsByCategory.map(
-          ({ id, name, description, category, picture }) => (
+          ({ id, name, description, category, picture, unitPrice }) => (
             <div
               key={id}
-              className={clsx(
-                "w-96 h-60 rounded-lg flex-shrink-0 flex-grow",
-                "bg-gray-400"
-              )}
-            ></div>
+              className="w-full sm:w-full md:w-1/3 xl:w-1/4 p-4 mb-14"
+            >
+              <div className="c-card block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden">
+                <div className="relative pb-48 overflow-hidden">
+                  <img
+                    className="absolute inset-0 h-full w-full object-cover"
+                    src={picture}
+                    alt={name}
+                  />
+                </div>
+
+                <div className="p-4">
+                  <span className="inline-block px-2 py-1 leading-none bg-orange-200 text-orange-800 rounded-full font-semibold uppercase tracking-wide text-xs">
+                    {category}
+                  </span>
+
+                  <h2 className="mt-2 mb-2 font-bold">{name}</h2>
+
+                  <p className="text-sm">{description}</p>
+
+                  <div className="mt-3 flex items-center">
+                    <span className="font-bold text-xl">{unitPrice}</span>&nbsp;
+                    <span className="text-sm font-semibold">€</span>
+                  </div>
+                </div>
+
+                <div className="p-2 border-t border-b text-xs text-gray-700 hover:cursor-pointer">
+                  <span className="flex items-center justify-center">
+                    <ShoppingCartIcon />
+                  </span>
+                </div>
+              </div>
+            </div>
           )
         )}
     </div>
