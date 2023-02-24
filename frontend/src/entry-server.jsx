f982fcc90afccd03ad1339e09cf5c8523d9f89c0
Flo Slv <florent3969@hotmail.fr>
2023-02-24 09:29:23 +0100

    🟦 feat: add Apollo Provider to both client-entry & server-entry

--- frontend/src/entry-server.jsx
+++ frontend/src/entry-server.jsx
@@ -1,12 +1,20 @@
 import ReactDOMServer from "react-dom/server";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { StaticRouter } from "react-router-dom/server";
 
 import Layout from "./components/Layout.jsx";
 
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
+
 export function render(url, context) {
   return ReactDOMServer.renderToString(
-    <StaticRouter location={url} context={context}>
-      <Layout />
-    </StaticRouter>
+    <ApolloProvider client={client}>
+      <StaticRouter location={url} context={context}>
+        <Layout />
+      </StaticRouter>
+    </ApolloProvider>
   );
 }
