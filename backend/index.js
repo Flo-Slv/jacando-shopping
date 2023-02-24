20d2886c4ffb3bd61970b99fdf7befa48d9814a3
Flo Slv <florent3969@hotmail.fr>
2023-02-24 09:04:17 +0100

    🟪 refactor: typo in backend

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { ApolloServer } from "@apollo/server";
-import { expressMiddleware } from "@apollo/server/express4";
-import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 import http from "http";
 import cors from "cors";
 import bodyParser from "body-parser";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
